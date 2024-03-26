473b83233e0bfa9f7adacf85dc43e1c68916333b
oluphunmmy <olufunmilayoagboola@gmail.com>
2024-03-26 05:09:22 +0100

    Addition of styles to my Bio_webpage using Tailwing CSS

---
+++ index.js
@@ -0,0 +1,95 @@
+// String
+// numbers
+// intergers
+// boolean
+// objects
+// array
+
+// How are you this morning
+//
+
+
+let greetings = "How are you today"
+const greetings_default = "Hi, how are you today!"
+
+console.log(greetings)
+
+let x = 500
+let y = 900
+
+const total = x + y
+
+console.log("The total is " + total)
+//boolean
+let class1 = true
+let class2 = true
+
+if (class1 == false){
+    console.log("class1 is free")
+}else{
+    console.log("class is not false")
+}
+
+// object
+SUV1 = {
+    name:"Tesla",
+    color:"Orange",
+    model: "v4g",
+    price: "3,000,000,",
+    weight: "500kg",
+}
+
+SUV2 = {
+    name:"Toyota",
+    color:"White",
+    model: "v2g",
+    price: "100,000,000,",
+    weight:"200kg",
+}
+console.log(SUV2)
+console.log(SUV2.price)
+
+// array
+
+
+//string
+
+let gender = "boy"
+
+console.log("I am a " + gender)
+
+//number
+let r = 600
+let p = 900
+
+const answer = r * p
+console.log("The answer is "  + answer)
+
+//boolean
+let fruit = true
+let cereal = false
+
+if (fruit == cereal){
+    console.log("It is fruity")
+}else{
+    console.log("It is Cereal!")
+}
+
+// object
+Girl = {
+    name:"Ada",
+    complexion:"fair",
+    age: "22",
+    height: "5.7",
+    weight: "40kg"
+}
+
+Boy = {
+    name:"Adeoye",
+    complrxion:"dark",
+    age: "25",
+    height: "6.3",
+    weight:"50kg"
+}
+console.log(Girl)
+console.log(Boy.weight)
